f4942137ca4305ee081b210b745c29e5df725631
Houssem Lachtar <lachtarhoussem4@gmail.com>
2025-03-18 23:42:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,32 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import sitemap from 'vite-plugin-sitemap';
 import { robots } from 'vite-plugin-robots';
+import VitePluginCopy from 'vite-plugin-copy'; // Import the plugin for copying files
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
   build: {
     sourcemap: true,
-    // terserOptions:
     chunkSizeWarningLimit: 1600,
   },
   plugins: [
     vue(),
     sitemap({
-      hostname: 'https://hetari.github.io/',
+      hostname: 'https://ibrahim-portfolio-jet.vercel.app',
       basePath: 'portfolio',
     }),
-    robots(),
+    robots({
+      robotsTxt: 'robots.production.txt', // Specify the correct path to robots.production.txt
+    }),
+    VitePluginCopy({
+      targets: [
+        {
+          src: 'robots.production.txt', // Path to your robots.production.txt
+          dest: 'dist', // Copy it to the dist folder
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
