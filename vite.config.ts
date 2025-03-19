c8fb26c3f3d966e96d558ca05e99f79b768f36c8
Houssem Lachtar <lachtarhoussem4@gmail.com>
2025-03-19 22:35:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { robots } from 'vite-plugin-robots';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/Ibrahim-portfolio/',
   build: {
     sourcemap: true,
     // terserOptions:
