8365fda8ddefab76da4ae207f8c23fdf4597aba0
Houssem Lachtar <lachtarhoussem4@gmail.com>
2025-03-23 23:05:51 +0100

    Update aboutMe.vue

--- src/components/sections/aboutMe.vue
+++ src/components/sections/aboutMe.vue
@@ -1,7 +1,7 @@
 <template>
   <section
     id="about-me-section"
-    class="common-padding relative z-10 overflow-y-clip rounded-b-3xl bg-[#0B0B0A] text-flax-smoke-200 shadow-2xl will-change-auto sm:mt-0 pt-[30%]"
+    class="common-padding relative z-10 overflow-y-clip rounded-b-3xl bg-[#0B0B0A] text-flax-smoke-200 shadow-2xl will-change-auto sm:mt-0 pt-[15%]"
   >
     <div class="md:column-gap grid grid-cols-12">
       <div class="hide-on-mobile overflow-hidden md:col-span-4">
@@ -88,8 +88,8 @@
 
   onBeforeMount(() => {
     aboutMe.value = textSplitterIntoChar(
-      'BunsinessAnalyst FrontendDeveloper Marketer Musician Filmaker',
-      true,
+      'Analyst Marketer Developer Musician Filmaker',
+      false,
       true,
     );
   });
