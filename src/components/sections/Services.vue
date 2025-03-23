11b12d03a5140d6effec4f860797592647bb4dd2
Houssem Lachtar <lachtarhoussem4@gmail.com>
2025-03-23 17:09:01 +0100

    Update Services.vue

--- src/components/sections/Services.vue
+++ src/components/sections/Services.vue
@@ -100,8 +100,8 @@
 
   const getStyle = computed(() => (index: number) => {
     if (window.innerWidth < 1024) {
-      if (index === 0) return 'top-[calc(20vh_+_0em)] mb-[17.75em]';
-      if (index === 1) return 'top-[calc(20vh_+_5.75em)] mb-[12.5em]';
+      if (index === 0) return 'top-[calc(20vh_+_0em)] mb-[17.5em]';
+      if (index === 1) return 'top-[calc(20vh_+_5.75em)] mb-[12em]';
       if (index === 2) return 'top-[calc(20vh_+_11.5em)] mb-[6.25em]';
     } else {
       if (index === 0) return 'top-[calc(9vh_-_1.25em)] mb-[18em]';
