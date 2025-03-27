1385afaccbcdbe8e63b9251b1780523712087596
Houssem Lachtar <lachtarhoussem4@gmail.com>
2025-03-27 20:36:05 +0000

    rh2

--- src/components/design/Footer.vue
+++ src/components/design/Footer.vue
@@ -61,7 +61,7 @@
     <div class="grid w-full grid-cols-12">
       <div class="col-span-7 place-content-center md:col-span-6">
         <h1 class="heading-4 sm:heading-2 font-bold leading-none">
-          © {{ new Date().getFullYear() }} HL <br />
+          © {{ new Date().getFullYear() }} Huy <br />
           All rights reserved.
         </h1>
       </div>
@@ -128,13 +128,12 @@
   const userLocalTime = ref('');
 
   onMounted(() => {
-  // San Francisco Time (Pacific Time Zone)
-  myLocalTime.value = moment.tz('America/Los_Angeles').format('h:mm:ss a');
-
-  setInterval(() => {
+    // Tunisia Time (local to you)
     myLocalTime.value = moment.tz('America/Los_Angeles').format('h:mm:ss a');
-  }, 1000);
-});
+
+    setInterval(() => {
+      myLocalTime.value = moment.tz('America/Los_Angeles').format('h:mm:ss a');
+    }, 1000);
 
 
     // User's local time (based on their location)
